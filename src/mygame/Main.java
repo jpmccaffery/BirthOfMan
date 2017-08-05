e6ccdd3935053eae7aedf705482317590efb8282
Jack McCaffery <jpmccaffery@gmail.com>
2017-08-04 21:44:23 -0500

    Jbullet

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -2,11 +2,16 @@ package mygame;
 
 import evolver.CreaturesMain;
 
+import com.bulletphysics.collision.shapes.SphereShape;
+
 // Test git on windows
 public class Main
 {
 	public static void main (String[] args_)
 	{
+		SphereShape sphere = new SphereShape (71);
+		System.out.println (sphere.getRadius ());
+
 		CreaturesMain app = new CreaturesMain ();
 
 		app.setPauseOnLostFocus (false);
